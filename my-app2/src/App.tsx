25a2ed09bf5ce505525268e6450b9879f96ca243
ollok <lhjbg0821@gmail.com>
2023-06-22 10:47:01 +0900

    props-box, circle

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
+import Box from "./components/Box";
+import Circle from "./components/Circle";
 
 const App: FC = () => {
-  return <div className="bg-red-300">Hello, React-TS!!</div>;
+  return (
+    <div>
+      <Box color="pink" width={200} height={200} />
+      <Box color="red" width={400} height={100} />
+      <Box color="blue" width={300} height={400} />
+      <Circle color="green" width={300} height={300} radius={9999} />
+    </div>
+  );
 };
 
 export default App;
---
+++ my-app2/src/components/Box.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+
+export interface BoxProps {
+  color: string;
+  width: number;
+  height: number;
+}
+
+//<interface Props> :  제네릭
+const Box: FC<BoxProps> = ({ color, width, height }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+        width,
+        height,
+        margin: 40,
+      }}
+    ></div>
+  );
+};
+
+export default Box;
